7e91eeaf826464682a445aad7577578ec5fa441b
𝐀𝐤𝐚𝐬𝐡 𝐃𝐚𝐤𝐬𝐡𝐰𝐚𝐧𝐬𝐡𝐢 : ZoxxOP <akp954834@gmail.com>
2025-10-18 21:55:52 +0530

    Update start.py

--- ShrutixMusic/utils/inline/start.py
+++ ShrutixMusic/utils/inline/start.py
@@ -24,18 +24,18 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{nand.username}?startgroup=true",
-            ),
-        ],
-        [
-            InlineKeyboardButton(
                 text=_["S_B_4"],
                 callback_data="settings_back_helper",
             ),
             InlineKeyboardButton(
                 text=_["S_B_2"],
                 url=config.SUPPORT_CHAT,
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{nand.username}?startgroup=true",
             ),
         ],
     ]
