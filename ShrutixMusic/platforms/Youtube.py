4c5d7432c896c6e3ca1b0020e8012c9cb4462b3f
RockMusicBot <githubmusicbot@gmail.com>
2025-11-18 10:38:29 +0530

    Update Youtube.py

--- ShrutixMusic/platforms/Youtube.py
+++ ShrutixMusic/platforms/Youtube.py
@@ -7,7 +7,7 @@ import requests
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from youtubesearchpython.__future__ import VideosSearch
+from py_yt import VideosSearch
 from ShrutixMusic.utils.database import is_on_off
 from ShrutixMusic import nand
 from ShrutixMusic.utils.formatters import time_to_seconds
