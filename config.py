ab4d03688ddcf8bb74bd395e388534adef32a7b4
RockMusicBot <githubmusicbot@gmail.com>
2025-11-25 15:55:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,8 +58,8 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 
 
 # Telegram audio and video file size limit (in bytes)
-TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 5368709120))
-TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 5368709120))
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 2147483648))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2147483648))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
 
